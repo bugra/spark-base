9d93f189d0181f2ad952592c933c686d8cfc2496
bugra <bugraa@live.com>
2016-11-26 15:56:36 -0500

    Use sunion instead of function call to get friends of friends and type annotations

--- src/main/scala/io/github/bugra/db/redis/models/Friend.scala
+++ src/main/scala/io/github/bugra/db/redis/models/Friend.scala
@@ -1,6 +1,8 @@
 package io.github.bugra.db.redis.models
 
+import akka.util.ByteString
 import io.github.bugra.db.redis.Red
+import redis.commands.TransactionBuilder
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
@@ -11,75 +13,82 @@ import scala.concurrent.duration._
 
 object Friend {
   val WAIT_TIME = 1.5
-  val redisTransaction = Red.redisTransaction
+  val redisTransaction: TransactionBuilder = Red.redisTransaction
 
-  def getFriends(playerId: String) = {
+  def getFriends(playerId: String): Set[String] = {
     val op = redisTransaction.smembers(playerId)
     redisTransaction.exec()
     val friendList = Await.result(op, WAIT_TIME seconds)
     friendList.map(_.utf8String).toSet - playerId
   }
 
-  def numberOfFriends(playerId: String) = {
+  def numberOfFriends(playerId: String): Int = {
     val op = redisTransaction.scard(playerId)
     redisTransaction.exec()
     Await.result(op, WAIT_TIME seconds).toInt
   }
 
-  def getFriendsOfFriends(playerId: String) = {
-    val friends = getFriends(playerId).toSeq
-    // TODO: Use sunion rather than function call, it would be faster
-    //redisTransaction.sunion(friends: _.*)
-    friends.flatMap(getFriends(_)).toSet - friends - Set(playerId)
+  def getFriendsOfFriends(playerId: String): Set[String] = {
+    // May not be a great idea to convert this into a sequence, could be used as a list
+    var friendsOfFriends = Set[String]()
+    val friends = getFriends(playerId).toList
+
+    if (friends.nonEmpty) {
+      val head::tail = friends
+      val op = redisTransaction.sunion(head, tail:_*)
+      friendsOfFriends = Await.result(op, WAIT_TIME seconds).map(_.utf8String).toSet
+
+    }
+    friendsOfFriends
   }
 
-  def getMutualFriends(playerId: String, subjectId: String) = {
+  def getMutualFriends(playerId: String, subjectId: String): Set[ByteString] = {
     val op = redisTransaction.sinter(playerId, subjectId)
     redisTransaction.exec()
     val mutualFriends = Await.result(op, WAIT_TIME seconds)
     mutualFriends.toSet
   }
 
-  def ping() = {
+  def ping(): String = {
     val op = redisTransaction.ping()
     redisTransaction.exec()
     val pong = Await.result(op, WAIT_TIME seconds)
     pong.toString
   }
 
-  def getUsers = {
+  def getUsers: Set[String] = {
     val op = redisTransaction.scan()
     redisTransaction.exec()
     val userList = Await.result(op, WAIT_TIME seconds)
     userList.data.toSet
   }
 
-  def size = {
+  def size: Int = {
     val op = redisTransaction.dbsize()
     redisTransaction.exec()
     val size = Await.result(op, WAIT_TIME seconds)
     size.toInt
   }
 
-  def flush = {
+  def flush: String = {
     val op = redisTransaction.flushdb()
     redisTransaction.exec()
     Await.result(op, WAIT_TIME seconds).toString
   }
 
-  def flushAll = {
+  def flushAll: String = {
     val op = redisTransaction.flushall()
     redisTransaction.exec()
     Await.result(op, WAIT_TIME seconds).toString
   }
 
-  def isExist(playerId: String, subjectId: String) = {
+  def isExist(playerId: String, subjectId: String): Boolean = {
     val op = redisTransaction.sismember(playerId, subjectId)
     redisTransaction.exec()
     Await.result(op, WAIT_TIME seconds)
   }
 
-  def insert(playerId: String, subjectIds: List[String]) = {
+  def insert(playerId: String, subjectIds: List[String]): Int = {
     val op = redisTransaction.sadd(playerId, subjectIds)
     redisTransaction.exec()
     Await.result(op, WAIT_TIME seconds).toInt
